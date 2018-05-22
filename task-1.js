bafbe37d3e517fa3fa887f0bf18cae989a42634a
Tim Watkins <timothy.watkins@gmail.com>
2018-05-22 13:42:36 -0600

    [Task 1] Added for-loop version of the Palindrome task

--- task-1.js
+++ task-1.js
@@ -41,9 +41,25 @@ function palindromeCheck(input) {
   // flatten case
   const lowerCaseInput = parsedInput.toLowerCase();
   // reverse the parsed/flattened input
-  const reversedInput = lowerCaseInput.split("").reverse().join(""); 
+  const reversedInput = lowerCaseInput.split("").reverse().join("");
   // check if the parsed, flattened input matches the reversed, parsed, flattened input
   return reversedInput === lowerCaseInput;
+}
+
+// Palindrome function using a for loop
+function palindromeWithLoop(input) {
+  // filter out non-alpha characters
+  const parsedInput = input.replace(/[^a-z]/gi, "");
+  // flatten case
+  const lowerCaseInput = parsedInput.toLowerCase();
+  // iterate through each character of the string, from the front and back at the same time, comparing the character from each side
+  const length = lowerCaseInput.length;
+  for (let i = 0; i < length/2; i++) {
+    // if the characters do not match, it is not a palindrome
+    if (lowerCaseInput[i] !== lowerCaseInput[length - 1 - i]) return false;
+  }
+  // the whole string was iterated without returning false, which means the string is a palindrome
+  return true;
 }
 
 function main() {
@@ -56,6 +72,16 @@ function main() {
   console.log("%cNegative Cases", "color: red");
   for (let input of negativeCases) {
     console.log(input + ": " + palindromeCheck(input));
+  }
+  // For Loop tests
+  console.log("%cLoop Version", "color: blue; font-size: large");
+  console.log("%cPositive Cases", "color: green");
+  for (let input of positiveCases) {
+    console.log(input + ": " + palindromeWithLoop(input));
+  }
+  console.log("%cNegative Cases", "color: red");
+  for (let input of negativeCases) {
+    console.log(input + ": " + palindromeWithLoop(input));
   }
 }
 
