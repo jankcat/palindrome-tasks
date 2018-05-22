22ec64e835ea599a9a6267364da2da8cc095246c
Tim Watkins <timothy.watkins@gmail.com>
2018-05-22 13:16:51 -0600

    Added test inputs to task 1

--- task-1.js
+++ task-1.js
@@ -1,1 +1,32 @@
+// Input Lists
+const positiveCases = [
+	"Bob",
+	"BOB",
+	"My age is 0, 0 si ega ym.",
+	"Bob bob",
+	"Bob's sbob",
+	"<!-- --!<",
+	"",
+	"\n",
+	"never odd or even",
+	"A man, a plan, a canal. Panama",
+	"r\nr",
+	"A but tuba.",
+	"Bo b ",
+	"o_o (: /-\\ :) o–o ",
+	"0_0 (: /-\\ :) 0–0 ",
+	"race car",
+	"12345",
+	"Amore, Roma",
+	"poop"
+];
 
+const negativeCases = [
+	"Robert",
+	"nope",
+	"almostomla",
+	"fda fadf 3rf3 aq4r < 1<$#!41,,`  ` 534 d",
+	"1 eye for of a 1 eye",
+	"fdsafd$#@$#@$fdsgGFS GFDSAG\nF Daf333412431431 431414#_!4 31431413sdadfsa",
+	"not a palindrome"
+];
